1669394a233d60b959833f37f2d8b49f3610b2d1
Mit Joshi <mitjoshi0207@gmail.com>
2024-04-10 17:08:55 +0530

    remove task is functional now

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function TodoForm() {
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 1000),
+            text: input
+        });
+        setInput('');
+    };
+
   return (
-    <div>
-
-    </div>
-  )
+    <form className='todo-form' onSubmit={handleSubmit}>
+        <input type='text' placeholder='Add a todo' value={input}
+        name="text" className='todo-input' onChange={handleChange}/>
+        <button className='todo-button'>Add Todo</button>
+    </form>
+  );
 }
 
 export default TodoForm
